7330d3d909842b4d53e05b0d874a5627b4c638d3
nikhilvishwa7 <123230101+nikhilvishwa7@users.noreply.github.com>
2023-04-20 23:50:07 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,8 +1,8 @@
-API_ID       = 
-API_HASH     = ""
-BOT_TOKEN    = ""
+API_ID       = 23640300
+API_HASH     = "b43066e201bfc457ad566e623fd74dab"
+BOT_TOKEN    = "5918067901:AAEMIifjJJ4YADpgWyb9Ff8xSQRqPnzIkIU"
 SESSION      = ""
-DATABASE_URI = ""
-LOG_CHANNEL  = -1001884373226
-ADMIN        = 1350642629
-CHANNEL      = "@GreyMatter_Bots"
+DATABASE_URI = "mongodb+srv://nikhilvishwa77:nikhil421@cluster0.re1dizx.mongodb.net/?retryWrites=true&w=majority"
+LOG_CHANNEL  = -1001606185333
+ADMIN        = 5149183428
+CHANNEL      = "@VisionBackup"
